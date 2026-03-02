2fce1dbe00a6b6e34ff494e47eff2eaf4ee4e622
BonkLordUs <gvvggg504@gmail.com>
2026-03-02 15:17:15 +0700

    Add files via upload

--- clear.bat
+++ clear.bat
@@ -1,75 +1,107 @@
 @echo off
-title Очистка кеша (Автоматический режим)
-color 0E
+setlocal enabledelayedexpansion
 
-:: Закрываем приложения (без вывода сообщений)
+:: Cache Cleaner for Discord, Steam and Browsers
+:: Runs completely in background
+
+:: Kill applications silently
 taskkill /F /IM discord.exe >nul 2>&1
 taskkill /F /IM steam.exe >nul 2>&1
 taskkill /F /IM chrome.exe >nul 2>&1
 taskkill /F /IM msedge.exe >nul 2>&1
 taskkill /F /IM firefox.exe >nul 2>&1
 taskkill /F /IM opera.exe >nul 2>&1
-taskkill /F /IM browser.exe >nul 2>&1
+taskkill /F /IM yandex.exe >nul 2>&1
+taskkill /F /IM brave.exe >nul 2>&1
+taskkill /F /IM vivaldi.exe >nul 2>&1
 
-:: Небольшая задержка, чтобы процессы точно закрылись
+:: Wait for processes to fully close
 timeout /t 2 /nobreak >nul
 
-:: Очистка Discord
+:: ========== DISCORD ==========
 if exist "%LocalAppData%\Discord" (
-    cd /d "%LocalAppData%\Discord"
-    for /d %%i in (Cache CodeCache GPUCache) do (
-        if exist "%%i" rmdir /s /q "%%i" >nul 2>&1
+    cd /d "%LocalAppData%\Discord" 2>nul
+    for %%i in (Cache CodeCache GPUCache) do (
+        if exist "%%i" (
+            rmdir /s /q "%%i" 2>nul
+        )
     )
 )
 
-:: Очистка Steam
+:: ========== STEAM ==========
 if exist "%LocalAppData%\Steam" (
-    cd /d "%LocalAppData%\Steam"
-    for /d %%i in (htmlcache appcache) do (
-        if exist "%%i" rmdir /s /q "%%i" >nul 2>&1
+    cd /d "%LocalAppData%\Steam" 2>nul
+    for %%i in (htmlcache appcache) do (
+        if exist "%%i" (
+            rmdir /s /q "%%i" 2>nul
+        )
     )
 )
 
-:: Очистка Google Chrome
-if exist "%LocalAppData%\Google\Chrome\User Data\Default" (
-    cd /d "%LocalAppData%\Google\Chrome\User Data\Default"
-    for /d %%i in (Cache Code Cache Storage) do (
-        if exist "%%i" rmdir /s /q "%%i" >nul 2>&1
+:: ========== GOOGLE CHROME ==========
+if exist "%LocalAppData%\Google\Chrome\User Data" (
+    for /d %%p in ("%LocalAppData%\Google\Chrome\User Data\*") do (
+        if exist "%%p\Cache" rmdir /s /q "%%p\Cache" 2>nul
+        if exist "%%p\Code Cache" rmdir /s /q "%%p\Code Cache" 2>nul
+        if exist "%%p\GPUCache" rmdir /s /q "%%p\GPUCache" 2>nul
     )
 )
 
-:: Очистка Microsoft Edge
-if exist "%LocalAppData%\Microsoft\Edge\User Data\Default" (
-    cd /d "%LocalAppData%\Microsoft\Edge\User Data\Default"
-    for /d %%i in (Cache Code Cache Storage) do (
-        if exist "%%i" rmdir /s /q "%%i" >nul 2>&1
+:: ========== MICROSOFT EDGE ==========
+if exist "%LocalAppData%\Microsoft\Edge\User Data" (
+    for /d %%p in ("%LocalAppData%\Microsoft\Edge\User Data\*") do (
+        if exist "%%p\Cache" rmdir /s /q "%%p\Cache" 2>nul
+        if exist "%%p\Code Cache" rmdir /s /q "%%p\Code Cache" 2>nul
     )
 )
 
-:: Очистка Mozilla Firefox
+:: ========== MOZILLA FIREFOX ==========
 if exist "%LocalAppData%\Mozilla\Firefox\Profiles" (
-    cd /d "%LocalAppData%\Mozilla\Firefox\Profiles"
-    for /d %%i in (*.default-release) do (
-        if exist "%%i\cache2" rmdir /s /q "%%i\cache2" >nul 2>&1
-        if exist "%%i\thumbnails" rmdir /s /q "%%i\thumbnails" >nul 2>&1
+    for /d %%p in ("%LocalAppData%\Mozilla\Firefox\Profiles\*") do (
+        if exist "%%p\cache2" rmdir /s /q "%%p\cache2" 2>nul
+        if exist "%%p\thumbnails" rmdir /s /q "%%p\thumbnails" 2>nul
+        if exist "%%p\offlinecache" rmdir /s /q "%%p\offlinecache" 2>nul
     )
 )
 
-:: Очистка Opera
+:: ========== OPERA ==========
 if exist "%LocalAppData%\Opera Software\Opera Stable" (
-    cd /d "%LocalAppData%\Opera Software\Opera Stable"
-    for /d %%i in (Cache Code Cache) do (
-        if exist "%%i" rmdir /s /q "%%i" >nul 2>&1
+    cd /d "%LocalAppData%\Opera Software\Opera Stable" 2>nul
+    for %%i in (Cache Code Cache) do (
+        if exist "%%i" rmdir /s /q "%%i" 2>nul
     )
 )
 
-:: Очистка Yandex Browser
-if exist "%LocalAppData%\Yandex\YandexBrowser\User Data\Default" (
-    cd /d "%LocalAppData%\Yandex\YandexBrowser\User Data\Default"
-    for /d %%i in (Cache Code Cache) do (
-        if exist "%%i" rmdir /s /q "%%i" >nul 2>&1
+:: ========== YANDEX BROWSER ==========
+if exist "%LocalAppData%\Yandex\YandexBrowser\User Data" (
+    for /d %%p in ("%LocalAppData%\Yandex\YandexBrowser\User Data\*") do (
+        if exist "%%p\Cache" rmdir /s /q "%%p\Cache" 2>nul
+        if exist "%%p\Code Cache" rmdir /s /q "%%p\Code Cache" 2>nul
     )
 )
 
-:: Завершение (без паузы)
+:: ========== BRAVE ==========
+if exist "%LocalAppData%\BraveSoftware\Brave-Browser\User Data" (
+    for /d %%p in ("%LocalAppData%\BraveSoftware\Brave-Browser\User Data\*") do (
+        if exist "%%p\Cache" rmdir /s /q "%%p\Cache" 2>nul
+        if exist "%%p\Code Cache" rmdir /s /q "%%p\Code Cache" 2>nul
+    )
+)
+
+:: ========== DISCORD PTB/CANARY ==========
+if exist "%LocalAppData%\DiscordPTB" (
+    cd /d "%LocalAppData%\DiscordPTB" 2>nul
+    for %%i in (Cache CodeCache GPUCache) do (
+        if exist "%%i" rmdir /s /q "%%i" 2>nul
+    )
+)
+
+if exist "%LocalAppData%\DiscordCanary" (
+    cd /d "%LocalAppData%\DiscordCanary" 2>nul
+    for %%i in (Cache CodeCache GPUCache) do (
+        if exist "%%i" rmdir /s /q "%%i" 2>nul
+    )
+)
+
+:: Exit silently
 exit
